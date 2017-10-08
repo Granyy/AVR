45dc8fde1637aa9e19d461c7f0226cd0c0bb657a
UwinLeo <leo@uwinloc.com>
2017-10-08 09:48:23 +0200

    Initial commit

---
+++ Blink/main.c
@@ -0,0 +1,18 @@
+// Blink on PINB0
+
+#define F_CPU 1000000
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void){
+  DDRB |= 1 << PINB0 ;
+  PORTB |= 1<<PINB0;
+  while(1){
+	PORTB ^= 1<<PINB0 ;
+	_delay_ms(1000);
+	
+  }
+  return 0;
+}
